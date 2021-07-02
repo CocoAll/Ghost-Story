9c49895b0170000221668896fa993a9607187e7c
CocoAll <corentin.all@gmail.com>
2021-07-02 15:31:38 +0200

    Chore: maj TMP PRO & start refacto

---
+++ Assets/Script/RoomState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Classe contenant les informations de l'état de la pièce.
+ */
+public class RoomState : MonoBehaviour
+{
+    [SerializeField]
+    private EnumRoomState state = EnumRoomState.HIDDEN;
+    [SerializeField]
+    private Transform startPosition;
+    [SerializeField]
+    private GameObject interactableParent;
+
+    public Transform StartPosition { get => startPosition; set => startPosition = value; }
+    public EnumRoomState State { get => state; set => state = value; }
+    public GameObject InteractableParent { get => interactableParent; set => interactableParent = value; }
+}
+
+public enum EnumRoomState
+{
+    HIDDEN,
+    CURRENT,
+    FINISHED
+}
